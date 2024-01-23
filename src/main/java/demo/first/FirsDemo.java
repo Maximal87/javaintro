89e13f6436d17c69d00fd0fcd4926e2066b38b70
maximal87 <maximal_87@yahoo.com>
2024-01-23 18:11:07 +0000

    Adding summ

---
+++ src/main/java/demo/first/FirsDemo.java
@@ -0,0 +1,7 @@
+package demo.first;
+
+public class FirsDemo {
+    public static void main(String[] args){
+        System.out.println("Hello Java");
+    }
+}
---
+++ src/main/java/hw01/summ.java
@@ -0,0 +1,8 @@
+package hw01;
+
+public class summ  {
+    public static void main(String[] args) {
+        System.out.printf("8 + 2");
+
+    }
+}
